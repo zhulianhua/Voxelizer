e34ff3e109cf0a300dda4edc848d8c2f0432af85
Lianhua Zhu <zhulianhua121@gmail.com>
2017-09-15 13:20:52 +0100

    retab, mix tab & space to space

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 
 int main(int argc, char* argv[])
  {
-	 // usage
-	 if (argc != 2) {
-		 cerr << "Usage voxelizer.exe <filename>.stl" << endl;
-		 exit(EXIT_FAILURE);
-	 }
-	string fname(argv[1]);
+     // usage
+     if (argc != 2) {
+         cerr << "Usage voxelizer.exe <filename>.stl" << endl;
+         exit(EXIT_FAILURE);
+     }
+    string fname(argv[1]);
 
-	// import geometry
-	Geometry geo(fname);
+    // import geometry
+    Geometry geo(fname);
     // The geometry bound, min/max corners coordinates will be reported.
     // These information will be used to determine the appropriate bound the grid
     // For this example we use the cube.stl file
@@ -28,41 +28,41 @@ int main(int argc, char* argv[])
     // Let's say: 
     // minCorner (-1,-1,-0.5)
     // maxCorner ( 1, 1, 1.5)
-	V3 gridMinCorner(-2.0, -2.0, -2.0);
-	V3 gridMaxCorner( 2.0,  2.0,  2.0);
+    V3 gridMinCorner(-2.0, -2.0, -2.0);
+    V3 gridMaxCorner( 2.0,  2.0,  2.0);
     V3 gridExtend = gridMaxCorner - gridMinCorner;
 
     // and we want a 33 voxels along X direction,
     // so dx = (1-(-1))/32;
-	double dx = gridExtend.x / 33;
+    double dx = gridExtend.x / 33;
     // and the grid number in each direction can be determined
     // Why not 32??? The voxillization implemntation is not robust enough.
     // If use 32, then the polar points causes wrong vexillization in the sphere axis
 
-	// set grid bounding corner
-	//int3 gridNum = int3(gridExtend.x / dx, gridExtend.y / dx, gridExtend.z /dx);
+    // set grid bounding corner
+    //int3 gridNum = int3(gridExtend.x / dx, gridExtend.y / dx, gridExtend.z /dx);
 
     // create the grid finally
-	GridBox grid(gridMinCorner, dx, gridNum);
+    GridBox grid(gridMinCorner, dx, gridNum);
 
-	// generate voxilzer from geometry and gridbox
-	Voxelizer vox(geo, grid);
+    // generate voxilzer from geometry and gridbox
+    Voxelizer vox(geo, grid);
 
-	// get flag
-	const char* flag = vox.get_flag();
-	gridNum = grid.get_gridNum();
+    // get flag
+    const char* flag = vox.get_flag();
+    gridNum = grid.get_gridNum();
 
-	// count solid flag
-	int count = 0;
-	for (int i = 0; i < gridNum.nx * gridNum.ny * gridNum.nz; i++)
-	{
-		//if (*(flag+i) == 0) count++;
-		if (flag[i] == 1) count++;
-	}
-	cout << "count of flag == 1 : " << count << endl;
+    // count solid flag
+    int count = 0;
+    for (int i = 0; i < gridNum.nx * gridNum.ny * gridNum.nz; i++)
+    {
+        //if (*(flag+i) == 0) count++;
+        if (flag[i] == 1) count++;
+    }
+    cout << "count of flag == 1 : " << count << endl;
 
-	// wirte vtk file of flag, use paraview to view the flag data
-	vox.write_vtk_image();
+    // wirte vtk file of flag, use paraview to view the flag data
+    vox.write_vtk_image();
 
     return 0;
 }
